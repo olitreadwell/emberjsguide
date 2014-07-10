ae47c65fe38d3da167468e8faede8610fcddb491
Oli Treadwell <olitreadwell@gmail.com>
2014-07-10 14:05:39 -0700

    feature: filter by completed todos

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function() {
     this.resource('todos', { path: '/' }, function (){
         //additional child routes
         this.route('active');
+        this.route('completed');
     });
 });
 
@@ -37,4 +38,15 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   renderTemplate: function(controller) {
     this.render('todos/index', {controller: controller});
   }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.filter('todo', function(todo) {
+            return todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    }
 });
